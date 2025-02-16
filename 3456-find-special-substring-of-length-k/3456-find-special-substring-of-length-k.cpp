d26ffff0f5944cd25402707e870c53d5243cd479
SaiPrabhakar08 <162802761+saiprabhakar08@users.noreply.github.com>
2025-02-16 14:26:13 +0530

    Time: 0 ms (100%), Space: 8.5 MB (100%) - LeetHub

---
+++ 3456-find-special-substring-of-length-k/3456-find-special-substring-of-length-k.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool hasSpecialSubstring(string s, int k) {
+        int count = 1;
+    if(s.size() == 1 && k == 1) return true;
+    for(int i = 1; i < s.size(); i++, count++){
+        if(s[i] != s[i-1] && count == k) return true;
+        if(s[i] != s[i-1]) count = 0;
+    }
+    return (count == k);
+    }
+};
