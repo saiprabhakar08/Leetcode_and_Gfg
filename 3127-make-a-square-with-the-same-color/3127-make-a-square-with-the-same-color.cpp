16de59ca8f9cf5e4a6473589cfada482028083a7
SaiPrabhakar08 <162802761+saiprabhakar08@users.noreply.github.com>
2025-02-28 20:47:50 +0530

    Time: 0 ms (100%), Space: 19.9 MB (72.8%) - LeetHub

--- 3127-make-a-square-with-the-same-color/3127-make-a-square-with-the-same-color.cpp
+++ 3127-make-a-square-with-the-same-color/3127-make-a-square-with-the-same-color.cpp
@@ -6,6 +6,7 @@ public:
         else if(grid[0][1]==grid[1][1] and grid[1][1]==grid[1][2]) return true;
         else if((grid[0][0]==grid[0][1] and grid[0][1]==grid[1][1]) or (grid[2][0]==grid[2][1] and grid[2][1]==grid[1][1])) return true;
         else if((grid[0][0]==grid[0][1] and grid[0][0]==grid[1][0]) or (grid[0][1]==grid[0][2] and grid[0][2]==grid[1][2]) or (grid[2][0]==grid[1][0] and grid[2][0]==grid[2][1]) or (grid[2][1]==grid[2][2] and grid[2][2]==grid[1][2])) return true;
+        else if((grid[0][1]==grid[0][2] and grid[0][1]==grid[1][1]) or (grid[1][1]==grid[2][1] and grid[2][1]==grid[2][2])) return true;
         else if(grid[1][0]==grid[1][1] and grid[1][1]==grid[2][1]) return true;
         else if((grid[1][0]==grid[1][1] and grid[1][0]==grid[2][0]) or (grid[1][1]==grid[1][2] and grid[1][2]==grid[2][2]))return true;
         else if(grid[1][1]==grid[1][2] and grid[1][1]==grid[2][1]) return true;
