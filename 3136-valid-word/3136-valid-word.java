4bbf4f3e716d519bf0ce233a46f80cd343f3df44
SaiPrabhakar08 <162802761+saiprabhakar08@users.noreply.github.com>
2025-07-15 15:45:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3136-valid-word/3136-valid-word.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isValid(String word) {
+        if(word.length() < 3) return false;
+        int vowels = 0, consonants = 0;
+        for(char it : word.toCharArray())
+        {
+            if(it == 'A' || it == 'E' || it == 'I' || it == 'O' || it == 'U' || it == 'a' || it == 'e' || it == 'i' || it == 'o' || it == 'u')
+            {
+                vowels++;
+            }
+            else if(it != '1' && it != '2' && it != '3' && it != '4' && it != '5' && it != '6' && it != '7' && it != '8' && it != '9' && it != '0' && it != '@' && it != '#' && it != '$')
+            {
+                consonants++;
+            }
+        }
+        if(vowels == 0 || consonants == 0) return false;
+        return true;
+                
+    }
+}
