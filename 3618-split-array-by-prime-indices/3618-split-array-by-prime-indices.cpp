a5acc5685b4838035595fac8c13f3cd7dc1baa6e
SaiPrabhakar08 <162802761+saiprabhakar08@users.noreply.github.com>
2025-07-26 20:04:36 +0530

    Time: 47 ms (29.08%), Space: 125.4 MB (98.58%) - LeetHub

---
+++ 3618-split-array-by-prime-indices/3618-split-array-by-prime-indices.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPrime(int n)
+    {
+        if(n < 2) return false;
+        for(int i = 2 ; i <= sqrt(n) ; i++)
+        {
+            if((n % i) == 0) return false;
+        }
+        return true;
+    }
+    long long splitArray(vector<int>& nums) {
+        long long sum1 = 0, sum2 = 0;
+        for(int i = 0 ; i < nums.size() ; i++)
+        {
+            if(isPrime(i)) sum1 += nums[i];
+            else sum2 += nums[i];
+        }
+        return abs(sum1-sum2);
+        
+    }
+};
