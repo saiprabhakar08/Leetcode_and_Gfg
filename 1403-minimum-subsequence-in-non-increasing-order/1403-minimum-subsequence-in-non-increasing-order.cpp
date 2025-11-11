555e61ca64ea3fef6eff7d742ed3196ccf860ee9
SaiPrabhakar08 <162802761+saiprabhakar08@users.noreply.github.com>
2025-11-11 20:22:49 +0530

    Time: 3 ms (30.12%), Space: 14.3 MB (50.12%) - LeetHub

--- 1403-minimum-subsequence-in-non-increasing-order/1403-minimum-subsequence-in-non-increasing-order.cpp
+++ 1403-minimum-subsequence-in-non-increasing-order/1403-minimum-subsequence-in-non-increasing-order.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     vector<int> minSubsequence(vector<int>& nums) {
-        long long total = 0;
-        for (int x : nums) total += x;
-
-        sort(nums.begin(), nums.end(), greater<int>()); // descending
-
-        vector<int> ans;
-        long long takenSum = 0;
-        for (int x : nums) {
-            ans.push_back(x);
-            takenSum += x;
-            if (takenSum > total - takenSum) break;
+        int totalSum = 0;
+        for(auto it : nums) totalSum += it;
+        sort(nums.begin(),nums.end());
+        int curr = 0;
+        vector<int>ans;
+        for(int i = nums.size() - 1; i >= 0; i--)
+        {
+            if(curr > (totalSum - curr)) return ans;
+            ans.push_back(nums[i]);
+            curr += nums[i];
         }
-        // ans is already in non-increasing (descending) order
         return ans;
         
     }
