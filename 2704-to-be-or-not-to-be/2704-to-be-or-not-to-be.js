bb52e9803e0d9f716644aef4dc439a1b22e1ac9c
SaiPrabhakar08 <162802761+saiprabhakar08@users.noreply.github.com>
2026-01-22 10:47:47 +0530

    Time: 41 ms (75.15%), Space: 53.9 MB (52.58%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe(val1)
+        {
+            if(val === val1) return true;
+            else throw "Not Equal";
+        },
+        notToBe(val1)
+        {
+            if(val !== val1) return true;
+            else throw "Equal";
+        }
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
