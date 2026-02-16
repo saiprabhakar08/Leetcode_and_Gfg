4b755894dac70962ad2ecd3522eee4f275f2518c
SaiPrabhakar08 <162802761+saiprabhakar08@users.noreply.github.com>
2026-02-16 23:43:14 +0530

    Time: 4 ms (29.28%), Space: 7.8 MB (100%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -4,10 +4,8 @@ public:
         uint32_t ans=0;
         for(int i=0;i<32;i++)
         {
-            // ans=ans+((n&(1<<i))*(pow(2,i)));
-            ans<<=1;
+            ans=ans<<1;
             ans|=(n&1);
-            // cout<<ans<<" ";
             n>>=1;
         }
         return ans;
