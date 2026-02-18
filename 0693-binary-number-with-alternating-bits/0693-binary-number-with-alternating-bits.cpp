1e1f45c1f62ebffcefef65b2861340607e308fb6
SaiPrabhakar08 <162802761+saiprabhakar08@users.noreply.github.com>
2026-02-18 23:53:56 +0530

    Time: 0 ms (100%), Space: 7.8 MB (30.66%) - LeetHub

---
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool hasAlternatingBits(int n) {
+        int bit = n & 1;
+        n >>= 1;
+        while(n)
+        {
+            if(bit == (n & 1)) return false;
+            else bit = n & 1;
+            n >>= 1;
+        }
+        return true;
+    }
+};
